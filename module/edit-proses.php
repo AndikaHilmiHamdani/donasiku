b5c8abd33c6431dbf21f6dad63e8f1abb728c006
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2020-06-16 03:59:02 +0700

    mengedit bug edit-proses

--- module/edit-proses.php
+++ module/edit-proses.php
@@ -8,11 +8,11 @@ if(isset($_POST['simpan'])){
 	include('koneksi.php');
 	
 	//jika tombol tambah benar di klik maka lanjut prosesnya
-		$id=  $_POST['id_donatur'] : '';
-		$nama = $_POST['nama_donatur'] : '';
-		$email = $_POST['email_donatur'] : '';
-		$norek =  $_POST['norek_donatur'] : '';
-		$jml =  $_POST['jml_donatur'] : '';
+		$id=  $_POST['id_donatur'] ;
+		$nama = $_POST['nama_donatur'] ;
+		$email = $_POST['email_donatur'] ;
+		$norek =  $_POST['norek_donatur'] ;
+		$jml =  $_POST['jml_donatur'] ;
 	
 	//melakukan query dengan perintah UPDATE untuk update data ke database dengan kondisi WHERE siswa_id='$id' <- diambil dari inputan hidden id
 	$update = mysqli_query($koneksi, "UPDATE donatur SET nama_donatur='$nama', email_donatur='$email', norek_donatur='$norek', jml_donatur='$jml', WHERE id_donatur='$id'") or die(mysqli_error());
